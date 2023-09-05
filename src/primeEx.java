65816404b7a39fa7fdf29bba78f31c23f9c2ec36
Deepak-0525 <deepaksaravanan45@gmail.com>
2023-09-04 21:17:33 -0700

    primeOrNot

---
+++ src/primeEx.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class primeEx {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=0,no=0;
+        System.out.println("Enter the no : ");
+        no=sc.nextInt();
+        i=2;
+        while(i<=no-1) {
+            if (no % i == 0) {
+                System.out.println("Not Prime ");
+                break;
+            }
+            else
+            i++;
+        }
+        if(i==no){
+                System.out.println("It is Prime ");
+        }
+    }
+}
