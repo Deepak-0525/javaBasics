36d3287487bf04babcdc388ad30f7944005bdf93
Deepak-0525 <deepaksaravanan45@gmail.com>
2023-09-06 13:25:05 -0700

    basicCode

---
+++ src/basicCode/JavaProgramStructure.java
@@ -0,0 +1,21 @@
+package basicCode;
+
+public class JavaProgramStructure {
+    public static void main(String[] args) {
+        // Display the Messsage
+        System.out.println("Hello");
+        // Display the Numbers
+        System.out.println("123");
+        System.out.println("4");
+        // Arithmetic Operators  "+, -, *, / "
+        // Addition
+        System.out.println(4 + 3);
+        // Subtraction
+        System.out.println(5 - 4);
+        // Multiplication
+        System.out.println(4 * 5);
+        // Division
+        System.out.println(6 / 7);
+
+    }
+}
