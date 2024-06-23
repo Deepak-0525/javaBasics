806b09d5ed8c38d20530a3c9f33d725eb0d859d4
Deepak-0525 <deepaksaravanan45@gmail.com>
2024-06-23 15:11:32 +0530

    Method function

---
+++ src/basicCode/MethodMain.java
@@ -0,0 +1,12 @@
+package basicCode;
+
+public class MethodMain {
+
+    static void myMethod(String fname){
+        System.out.println("First Name is : "+fname);
+    }
+
+    public static void main(String[]args){
+        myMethod("Vidula");
+    }
+}
